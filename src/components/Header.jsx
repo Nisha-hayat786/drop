48aa8c504429d7ba923015fabced13f9df22eb05
Nisha-hayat786 <nisha6895540@gmail.com>
2025-08-02 00:56:12 +0500

    venue dashboard ui completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FaBell, FaRegBell } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
+import avatar from '../assets/images/user.svg';
 
 const Header = () => {
   const navigate = useNavigate()
@@ -12,7 +13,7 @@ const Header = () => {
         <span className="mr-8 text-xl"><FaRegBell /></span>
         <span className="mr-3 text-sm">Nick McMillan</span>
         <span className="w-10 h-10 rounded-full bg-gray-200 inline-block overflow-hidden me-4 cursor-pointer" onClick={() => navigate("/profile")}>
-          <img src="/src/assets/images/user.svg" alt="avatar" className="w-full h-full object-cover" />
+          <img src={avatar} alt="avatar" className="w-full h-full object-cover" />
         </span>
       </div>
     </header>
